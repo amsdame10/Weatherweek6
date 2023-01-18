31623470c5ce28d1488298d0a89f23cec3658ee4
Andrea Smith <122823920+amsdame10@users.noreply.github.com>
2023-01-17 22:31:44 -0800

    Add files via upload

---
+++ Weather-app-6.html/src/index.js
@@ -0,0 +1,70 @@
+const apt={
+key:"8402ccd9e55983fce71eeeaa1d2bd1fc"
+baseURL: "https://api.openweathermap.org/data/2.5/"
+};
+
+const searchbox = document.querySelector(".search-box");
+searchbox.addEventListener("keypress", setQuery);
+
+function setQuery(event) {
+  if (event.keyCode === 13) {
+    getResults(searchbox.value);
+  }
+}
+
+function getResults(query) {
+  const url = `${api.baseURL}weather?q=${query}&units=imperial&appid=${api.key}`;
+  fetch(url)
+    .then((weather) => {
+      return weather.json();
+    })
+    .then(displayResults);
+}
+function displayResults(weather) {
+  let city = document.querySelector(".location .city");
+  city.innerText = `${weather.name}`;
+
+  let now = new Date();
+  let date = document.querySelector(".location .date");
+  date.innerText = dateBuilder(now);
+
+  let hilow = document.querySelector(".hi-low");
+  hilow.innerText = `${Math.round(weather.main.temp_min)}°f / ${Math.round(
+    weather.main.temp_max
+  )}°f`;
+
+  let temp = document.querySelector(".current .temp");
+  temp.innerHTML = `${Math.round(weather.main.temp)}<span>°f</span>`;
+}
+function dateBuilder(d) {
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let day = days[d.getDay()];
+  let date = d.getDate();
+  let month = months[d.getMonth()];
+  let year = d.getFullYear();
+
+  return `${day} ${date} ${month} ${year}`;
+}
